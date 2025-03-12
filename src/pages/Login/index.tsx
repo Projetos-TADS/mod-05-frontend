5f6c0eb763163e92816a9f8103f4cc6fe17f31b2
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-12 19:03:23 -0300

    Altera página Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
-import { LoginForm } from "../../components/signinForm";
+import { Link } from "react-router";
+import { LoginForm } from "../../components/SigninForm";
 
 export const LoginPage = () => {
   return (
     <>
       <h1>Rota de login</h1>
       <LoginForm />
+      <Link to="/signup">Cadastre-se</Link>
     </>
   );
 };
