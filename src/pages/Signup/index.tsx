dc06d6cabbececd8854e43fcab8e91d717d6abc3
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-12 19:03:40 -0300

    Altera página Signup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,10 +1,12 @@
-import { SignupForm } from "../../components/signupForm";
+import { Link } from "react-router";
+import { SignupForm } from "../../components/SignupForm";
 
 export const SignupPage = () => {
   return (
     <>
       <h1>Rota de cadastro</h1>
       <SignupForm />
+      <Link to="/">Voltar</Link>
     </>
   );
 };
