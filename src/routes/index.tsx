96299c5d1933a9b19ab0e25561ab18ac5199936e
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-12 19:01:53 -0300

    Cria ProtectedRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,21 @@ import { LoginPage } from "../pages/Login";
 import { SignupPage } from "../pages/Signup";
 import { MoviesPage } from "../pages/Movies";
 import { InfoPage } from "../pages/Info";
+import { ProtectedRoutes } from "../components/ProtectedRoutes";
+import { NotFound } from "../pages/NotFound";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route path="/movies" element={<MoviesPage />} />
-      <Route path="/info/:movieId" element={<InfoPage />} />
+      <Route path="/movies" element={<ProtectedRoutes />}>
+        <Route index element={<MoviesPage />} />
+      </Route>
+      <Route path="/info/:movieId" element={<ProtectedRoutes />}>
+        <Route index element={<InfoPage />} />
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
