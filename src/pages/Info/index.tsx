0b9367a8f75baca06cea98521433ba9e55247676
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-13 20:10:11 -0300

    Altera PageInfo

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,21 +1,3 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { api } from "../../services/api";
-
 export const InfoPage = () => {
-  const { movieId } = useParams();
-  const [movie, setMovie] = useState<{ movieId: string; title: string }[]>([]);
-
-  useEffect(() => {
-    async function getMovies() {
-      const response = await api.get(`/movies/${movieId}`);
-
-      setMovie(response.data);
-      console.log(response.data);
-    }
-
-    getMovies();
-  }, []);
-
   return <h1>Rota Info</h1>;
 };
