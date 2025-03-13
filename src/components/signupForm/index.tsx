e0d744c930cd6303f1ab717d3fc5273a0cc7c7e7
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-13 12:46:00 -0300

    Altera SignupForm

--- src/components/signupForm/index.tsx
+++ src/components/signupForm/index.tsx
@@ -1,12 +1,9 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { UserContext } from "../../providers/UserContext";
-import { useContext } from "react";
-
-export interface ISignupFormData {
-  name: string;
-  email: string;
-  password: string;
-}
+import { useContext, useState } from "react";
+import { Input } from "../Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupFormSchema, TSignupFormValues } from "./signupFormSchema";
 
 export const SignupForm = () => {
   const { userSignup } = useContext(UserContext);
@@ -16,21 +13,43 @@ export const SignupForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ISignupFormData>();
+  } = useForm<TSignupFormValues>({
+    resolver: zodResolver(signupFormSchema),
+  });
 
-  const submit: SubmitHandler<ISignupFormData> = (formData) => {
+  const submit: SubmitHandler<TSignupFormValues> = (formData) => {
     userSignup(formData, setLoading);
   };
 
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <input type="text" placeholder="Seu nome" {...register("name")} disabled={loading} />
-      <input type="email" placeholder="Seu email" {...register("email")} disabled={loading} />
-      <input
+      <Input
+        type="text"
+        placeholder="Seu nome"
+        {...register("name")}
+        disabled={loading}
+        error={errors.name}
+      />
+      <Input
+        type="email"
+        placeholder="Seu email"
+        {...register("email")}
+        disabled={loading}
+        error={errors.email}
+      />
+      <Input
         type="password"
         placeholder="Seu password"
         {...register("password")}
         disabled={loading}
+        error={errors.password}
+      />
+      <Input
+        type="password"
+        placeholder="Confirme seu password"
+        {...register("confirmPassword")}
+        disabled={loading}
+        error={errors.confirmPassword}
       />
       <button type="submit" disabled={loading}>
         {loading ? "Cadastrando..." : "Cadastrar"}
