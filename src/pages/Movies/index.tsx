3a72a4094e9f0378081efe896f41e1ffd860b070
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-13 20:09:45 -0300

    Altera PageMovies

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,18 +1,20 @@
 import { useContext } from "react";
 import { UserContext } from "../../providers/UserContext";
 import { MoviesContext } from "../../providers/MoviesContext";
-import { CreateNewMovieForm } from "../../components/CreateMovieForm";
-import { UserEditForm } from "../../components/UserEditForm";
+import { CreateNewMovieForm } from "../../components/MovieCreateForm";
+import { UserEditForm } from "../../components/UserUpdateForm";
+import { MovieUpdateForm } from "../../components/MovieUpdateForm";
 
 export const MoviesPage = () => {
-  const { userLogout, user } = useContext(UserContext);
-  const { moviesList, deleteMovie } = useContext(MoviesContext);
+  const { userLogout, userDelete, user } = useContext(UserContext);
+  const { moviesList, movieDelete, modalMovieEdit, setmodalMovieEdit } = useContext(MoviesContext);
 
   return (
     <main>
       <header>
         <span>{user?.name}</span>
         <button onClick={() => userLogout()}>Logout</button>
+        <button onClick={() => user?.userId && userDelete(user.userId)}>Deletar</button>
         <h2>Editar Usuário</h2>
         <UserEditForm />
       </header>
@@ -22,7 +24,9 @@ export const MoviesPage = () => {
           {moviesList?.map((currentMovie) => (
             <li key={currentMovie.movieId}>
               <h3>{currentMovie.title}</h3>
-              <button onClick={() => deleteMovie(currentMovie.movieId)}>Deletar</button>
+              <button onClick={() => setmodalMovieEdit(true)}>Editar</button>
+              <button onClick={() => movieDelete(currentMovie.movieId)}>Deletar</button>
+              {modalMovieEdit ? <MovieUpdateForm movie={currentMovie} /> : null}
             </li>
           ))}
         </ul>
@@ -30,9 +34,6 @@ export const MoviesPage = () => {
       <section>
         <h1>CRIAR NOVO FILME</h1>
         <CreateNewMovieForm />
-      </section>
-      <section>
-        <h1>EDITAR NOVO FILME</h1>
       </section>
     </main>
   );
