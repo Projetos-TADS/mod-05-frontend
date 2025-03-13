0dd2b74dd02b41dbc0e3f273f8866b1af70884ac
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-13 12:46:17 -0300

    Altera Page movies

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,22 +1,10 @@
-import { useContext, useEffect, useState } from "react";
-import { api } from "../../services/api";
-import { Link } from "react-router";
+import { useContext } from "react";
 import { UserContext } from "../../providers/UserContext";
+import { MoviesContext } from "../../providers/MoviesContext";
 
 export const MoviesPage = () => {
   const { userLogout, user } = useContext(UserContext);
-  // const [movies, setMovies] = useState<{ movieId: string; title: string }[]>([]);
-
-  // useEffect(() => {
-  //   async function getMovies() {
-  //     const response = await api.get("/movies");
-
-  //     setMovies(response.data.data);
-  //     console.log(response.data.data);
-  //   }
-
-  //   getMovies();
-  // }, []);
+  const { moviesList } = useContext(MoviesContext);
 
   return (
     <main>
@@ -24,19 +12,15 @@ export const MoviesPage = () => {
         <span>{user?.name}</span>
         <button onClick={() => userLogout()}>Logout</button>
       </header>
-      {/* <section>
+      <section>
         <ul>
-          {movies.length > 0 ? (
-            movies.map((movie) => (
-              <li key={movie.movieId}>
-                <Link to={`/info/${movie.movieId}`}>{movie.title}</Link>
-              </li>
-            ))
-          ) : (
-            <li>{movies.length === 0 ? "Loading movies..." : "No movies available"}</li>
-          )}
+          {moviesList?.map((currentMovie) => (
+            <li key={currentMovie.movieId}>
+              <h3>{currentMovie.title}</h3>
+            </li>
+          ))}
         </ul>
-      </section> */}
+      </section>
     </main>
   );
 };
