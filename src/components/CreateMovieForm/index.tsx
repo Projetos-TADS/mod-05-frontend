35cbbcf3d37c5ca60929237de0810888742b2827
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-13 16:59:18 -0300

    Cria componente de create movie

---
+++ src/components/CreateMovieForm/index.tsx
@@ -0,0 +1,76 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from "../Input";
+import { createMovieFormSchema, TCreateMovieFormValues } from "./createMovieFormSchema";
+import { useContext, useState } from "react";
+import { MoviesContext } from "../../providers/MoviesContext";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const CreateNewMovieForm = () => {
+  const [loading, setLoading] = useState(false);
+  const { createNewMovie } = useContext(MoviesContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TCreateMovieFormValues>({
+    resolver: zodResolver(createMovieFormSchema),
+  });
+
+  const submit: SubmitHandler<TCreateMovieFormValues> = (formData) => {
+    createNewMovie(formData, setLoading);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <Input
+        type="text"
+        {...register("title")}
+        placeholder="Título do filme"
+        disabled={loading}
+        error={errors.title}
+      />
+      <Input
+        type="text"
+        {...register("description")}
+        placeholder="Descrição do filme"
+        disabled={loading}
+        error={errors.description}
+      />
+      <Input
+        type="number"
+        {...register("releaseYear")}
+        placeholder="Ano de lançamento do filme"
+        disabled={loading}
+        error={errors.releaseYear}
+      />
+      <Input
+        type="number"
+        {...register("duration")}
+        placeholder="Duração do filme"
+        disabled={loading}
+        error={errors.duration}
+      />
+      <Input
+        type="number"
+        min="0"
+        max="5"
+        step="0.1"
+        {...register("rating")}
+        placeholder="Avaliação do filme"
+        disabled={loading}
+        error={errors.rating}
+      />
+      <Input
+        type="text"
+        {...register("urlImage")}
+        placeholder="URL da imagem do filme"
+        disabled={loading}
+        error={errors.urlImage}
+      />
+      <button type="submit" disabled={loading}>
+        {loading ? "Cadastrando..." : "Cadastrar"}
+      </button>
+    </form>
+  );
+};
