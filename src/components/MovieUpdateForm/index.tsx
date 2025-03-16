d75e4e3573c5648b8de93a406923e68264e099bd
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-16 17:39:12 -0300

    Altera componente movie update forms

--- src/components/MovieUpdateForm/index.tsx
+++ src/components/MovieUpdateForm/index.tsx
@@ -1,17 +1,17 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../Input";
 import { useContext } from "react";
-import { IMovie, MoviesContext } from "../../providers/MoviesContext";
+import { IMovie, MovieContext } from "../../providers/MovieContext";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { movieUpdateFormSchema, TMovieUpdateFormValues } from "./movieupdateFormSchema";
 
 interface MovieUpdateFormProps {
   movie: IMovie;
+  onClose: () => void;
 }
 
 export const MovieUpdateForm = ({ movie }: MovieUpdateFormProps) => {
-  // const [loading, setLoading] = useState(false);
-  const { movieUpdate, setmodalMovieEdit } = useContext(MoviesContext);
+  const { movieUpdate, setModalMovieEdit } = useContext(MovieContext);
 
   const {
     register,
@@ -28,39 +28,35 @@ export const MovieUpdateForm = ({ movie }: MovieUpdateFormProps) => {
 
     if (Object.keys(filteredData).length > 0) movieUpdate(filteredData, movie.movieId);
 
-    setmodalMovieEdit(false);
+    setModalMovieEdit(false);
   };
 
   return (
     <>
-      <button onClick={() => setmodalMovieEdit(false)}>Fechar</button>
+      <button onClick={() => setModalMovieEdit(false)}>Fechar</button>
       <form onSubmit={handleSubmit(submit)}>
         <Input
           type="text"
           {...register("title")}
           placeholder={movie?.title || "Título do filme"}
-          // disabled={loading}
           error={errors.title}
         />
         <Input
           type="text"
           {...register("description")}
           placeholder={movie?.description || "Descrição do filme"}
-          // disabled={loading}
           error={errors.description}
         />
         <Input
           type="number"
           {...register("releaseYear")}
           placeholder={movie?.releaseYear.toString() || "Ano de lançamento do filme"}
-          // disabled={loading}
           error={errors.releaseYear}
         />
         <Input
           type="number"
           {...register("duration")}
           placeholder={movie?.duration.toString() || "Duração do filme"}
-          // disabled={loading}
           error={errors.duration}
         />
         <Input
@@ -70,22 +66,15 @@ export const MovieUpdateForm = ({ movie }: MovieUpdateFormProps) => {
           step="0.1"
           {...register("rating")}
           placeholder={movie?.rating.toString() || "Avaliação do filme"}
-          // disabled={loading}
           error={errors.rating}
         />
         <Input
           type="text"
           {...register("urlImage")}
           placeholder={movie?.urlImage || "URL da imagem do filme"}
-          // disabled={loading}
           error={errors.urlImage}
         />
-        <button
-          type="submit"
-          // disabled={loading}
-        >
-          Atualizar
-        </button>
+        <button type="submit">Atualizar</button>
       </form>
     </>
   );
