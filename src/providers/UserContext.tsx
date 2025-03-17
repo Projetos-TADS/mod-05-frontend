c4da6f053bf49ebaa66f54063fe5aa7fe51ebb23
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-17 14:21:58 -0300

    altera userContext

--- src/providers/UserContext.tsx
+++ src/providers/UserContext.tsx
@@ -16,6 +16,21 @@ interface IUser {
   admin: boolean;
 }
 
+interface IFavoriteResponse {
+  userId: string;
+  name: string;
+  email: string;
+  cpf: string;
+  admin: boolean;
+  favoriteList: IFavorite[];
+}
+
+interface IFavorite {
+  movieId: string;
+  favoriteMovieId: string;
+  addedDate: string;
+}
+
 interface IUserContext {
   user: IUser | null;
   userSignin: (
@@ -32,6 +47,11 @@ interface IUserContext {
     setLoading: React.Dispatch<React.SetStateAction<boolean>>
   ) => Promise<void>;
   userDelete: (userId: string) => Promise<void>;
+  favoritesList: IFavorite[];
+  setFavoritesList: React.Dispatch<React.SetStateAction<IFavorite[]>>;
+  favoritesLoad: () => Promise<void>;
+  addMovieToFavorite: (movieId: string) => Promise<void>;
+  removeMovieFromFavorite: (favoriteId: string) => Promise<void>;
 }
 
 interface IUserSigninResponse {
@@ -44,6 +64,23 @@ export const UserContext = createContext({} as IUserContext);
 export const UserProvider = ({ children }: IUserProviderProps) => {
   const [user, setUser] = useState<IUser | null>(null);
   const navigate = useNavigate();
+  const [favoritesList, setFavoritesList] = useState<IFavorite[]>([]);
+
+  const favoritesLoad = async () => {
+    const userToken: string | null = localStorage.getItem("@USERTOKEN");
+    const userId: string | null = localStorage.getItem("@USERID");
+
+    try {
+      const { data } = await api.get<IFavoriteResponse>(`/favorites/all/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+      setFavoritesList(data.favoriteList);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const userToken: string | null = localStorage.getItem("@USERTOKEN");
@@ -149,9 +186,58 @@ export const UserProvider = ({ children }: IUserProviderProps) => {
     }
   };
 
+  const addMovieToFavorite = async (movieId: string) => {
+    const userToken: string | null = localStorage.getItem("@USERTOKEN");
+    const userId: string | null = localStorage.getItem("@USERID");
+
+    try {
+      await api.post<void>(
+        `/favorites/${userId}`,
+        { movieId },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+
+      console.log("Filme favoritado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeMovieFromFavorite = async (favoriteId: string) => {
+    const userToken: string | null = localStorage.getItem("@USERTOKEN");
+
+    try {
+      await api.delete<void>(`/favorites/${favoriteId}`, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+
+      console.log("Filme desfavoritado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <UserContext.Provider
-      value={{ userSignin, userSignup, userLogout, userUpdate, userDelete, user }}
+      value={{
+        userSignin,
+        userSignup,
+        userLogout,
+        userUpdate,
+        userDelete,
+        user,
+        setFavoritesList,
+        favoritesList,
+        favoritesLoad,
+        addMovieToFavorite,
+        removeMovieFromFavorite,
+      }}
     >
       {children}
     </UserContext.Provider>
