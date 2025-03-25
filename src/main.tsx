5154da0aecf490a1609cdd26c0405b45cda83595
Amanda Dolci <amanda.dolci@gmail.com>
2025-03-24 21:53:11 -0300

    Instala e configura antdesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "antd/dist/reset.css";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { UserProvider } from "./providers/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</BrowserRouter>
+	</StrictMode>
 );
