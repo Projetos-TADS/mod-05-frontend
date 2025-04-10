717ac09d4b4b6186c761ae4be9be71c126a37b35
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 20:32:52 -0300

    Adiciona validação de email ao login

--- src/components/signinForm/loginFormSchema.ts
+++ src/components/signinForm/loginFormSchema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  email: z.string().nonempty("E-mail is required"),
-  password: z.string().nonempty("Password is required"),
+	email: z.string().nonempty("E-mail is required").email("Invalid e-mail format"),
+	password: z.string().nonempty("Password is required"),
 });
 
 export type TLoginFormValues = z.infer<typeof loginFormSchema>;
