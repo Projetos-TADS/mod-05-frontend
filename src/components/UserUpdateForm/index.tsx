44000bfc98978bca593dc1b7270be38c9f3a5a6e
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 16:40:16 -0300

    Estiliza formulário de edição de user

--- src/components/UserUpdateForm/index.tsx
+++ src/components/UserUpdateForm/index.tsx
@@ -1,65 +1,125 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import { UserContext } from "../../providers/UserContext";
 import { useContext, useState } from "react";
-import { Input } from "../Input";
+import { Controller, useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Form, Input, message } from "antd";
+import { UserContext } from "../../providers/UserContext";
 import { TUserUpdateFormValues, userUpdateSchema } from "./userUpdateFormSchema";
 
-export const UserEditForm = () => {
-  const { userUpdate, user } = useContext(UserContext);
-  const [loading, setLoading] = useState(false);
+export const UserEditForm = ({ onSuccess }: { onSuccess: () => void }) => {
+	const { userUpdate, user } = useContext(UserContext);
+	const [loading, setLoading] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<TUserUpdateFormValues>({
-    resolver: zodResolver(userUpdateSchema),
-  });
+	const { control, handleSubmit } = useForm<TUserUpdateFormValues>({
+		resolver: zodResolver(userUpdateSchema),
+		defaultValues: {
+			name: user?.name || "",
+			email: user?.email || "",
+		},
+	});
 
-  const submit: SubmitHandler<TUserUpdateFormValues> = (newUserData) => {
-    const filteredData = Object.fromEntries(
-      Object.entries(newUserData).filter(
-        ([key, value]) => value !== undefined && value !== "" && key !== "confirmPassword"
-      )
-    );
+	const onSubmit: SubmitHandler<TUserUpdateFormValues> = async newUserData => {
+		try {
+			setLoading(true);
 
-    if (Object.keys(filteredData).length > 0) userUpdate(filteredData, setLoading);
-  };
+			const filteredData = Object.fromEntries(
+				Object.entries(newUserData).filter(
+					([key, value]) => value !== undefined && value !== "" && key !== "confirmPassword"
+				)
+			);
 
-  return (
-    <form onSubmit={handleSubmit(submit)}>
-      <Input
-        type="text"
-        placeholder={user?.name || "Seu nome"}
-        {...register("name")}
-        disabled={loading}
-        error={errors.name}
-      />
-      <Input
-        type="email"
-        placeholder={user?.email || "Seu e-mail"}
-        {...register("email")}
-        disabled={loading}
-        error={errors.email}
-      />
-      <Input
-        type="password"
-        placeholder="Seu password"
-        {...register("password")}
-        disabled={loading}
-        error={errors.password}
-      />
-      <Input
-        type="password"
-        placeholder="Confirme seu password"
-        {...register("confirmPassword")}
-        disabled={loading}
-        error={errors.confirmPassword}
-      />
-      <button type="submit" disabled={loading}>
-        {loading ? "Enviando..." : "Enviar"}
-      </button>
-    </form>
-  );
+			if (Object.keys(filteredData).length > 0) {
+				await userUpdate(filteredData, setLoading);
+				message.success("Dados atualizados com sucesso!");
+				onSuccess();
+			}
+		} catch (error) {
+			message.error("Erro ao atualizar dados");
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
+			<Controller
+				name="name"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Form.Item
+						label="Nome"
+						validateStatus={fieldState.error ? "error" : ""}
+						help={fieldState.error?.message}>
+						<Input
+							{...field}
+							placeholder="Seu nome"
+							disabled={loading}
+							status={fieldState.error ? "error" : ""}
+						/>
+					</Form.Item>
+				)}
+			/>
+
+			<Controller
+				name="email"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Form.Item
+						label="E-mail"
+						validateStatus={fieldState.error ? "error" : ""}
+						help={fieldState.error?.message}>
+						<Input
+							{...field}
+							placeholder="Seu e-mail"
+							disabled={loading}
+							status={fieldState.error ? "error" : ""}
+						/>
+					</Form.Item>
+				)}
+			/>
+
+			<Controller
+				name="password"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Form.Item
+						label="Nova Senha"
+						validateStatus={fieldState.error ? "error" : ""}
+						help={fieldState.error?.message}>
+						<Input
+							type="password"
+							{...field}
+							status={fieldState.error ? "error" : ""}
+							placeholder="Sua senha"
+							disabled={loading}
+						/>
+					</Form.Item>
+				)}
+			/>
+
+			<Controller
+				name="confirmPassword"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Form.Item
+						label="Confirme a Senha"
+						validateStatus={fieldState.error ? "error" : ""}
+						help={fieldState.error?.message}>
+						<Input
+							type="password"
+							{...field}
+							status={fieldState.error ? "error" : ""}
+							placeholder="Sua senha"
+							disabled={loading}
+						/>
+					</Form.Item>
+				)}
+			/>
+
+			<Form.Item>
+				<Button type="primary" htmlType="submit" loading={loading} block>
+					{loading ? "Atualizando..." : "Atualizar dados"}
+				</Button>
+			</Form.Item>
+		</Form>
+	);
 };
