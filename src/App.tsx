14b9ade289a0d62a70c0c3a933449bfd5187502e
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 19:55:39 -0300

    Instala react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import { MainRoutes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const App = () => {
-  return <MainRoutes />;
+	return (
+		<>
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar
+				closeOnClick
+				theme="light"
+			/>
+			<MainRoutes />
+		</>
+	);
 };
