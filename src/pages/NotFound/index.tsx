ddf02a4a27a20b870c7133cc5e2601baee17dec3
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 16:08:34 -0300

    Estiliza PageNotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,40 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router";
+import { Button } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+import Result from "antd/es/result";
+import Space from "antd/es/space";
+import Typography from "antd/es/typography";
+import { UserContext } from "../../providers/UserContext";
+
+const { Text } = Typography;
+
 export const NotFound = () => {
-  return <h1>Página não encontrada</h1>;
+	const navigate = useNavigate();
+	const { user } = useContext(UserContext);
+
+	const handleReturn = () => {
+		navigate(user ? "/movies" : "/");
+	};
+
+	return (
+		<Result
+			status="404"
+			title="Página não encontrada"
+			subTitle={
+				<>
+					<Text type="secondary">Desculpe, a página que você visitou não existe.</Text>
+					<br />
+					<Text keyboard>Erro 404</Text>
+				</>
+			}
+			extra={
+				<Space>
+					<Button type="primary" icon={<HomeOutlined />} onClick={handleReturn}>
+						{user ? "Página Inicial" : "Ir para login"}
+					</Button>
+				</Space>
+			}
+		/>
+	);
 };
