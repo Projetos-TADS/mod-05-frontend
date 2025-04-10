cd12aedb997635e75953e24112fd3cf1f4fa4663
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 16:05:03 -0300

    Adiciona restante de interfaces de tipagem do antd

--- src/antd.d.ts
+++ src/antd.d.ts
@@ -13,6 +13,7 @@ declare module "antd" {
 		label?: React.ReactNode;
 		validateStatus?: "" | "success" | "warning" | "error" | "validating";
 		help?: React.ReactNode;
+		placeholder?: string;
 	}
 
 	export interface AlertProps {
@@ -24,11 +25,34 @@ declare module "antd" {
 
 	export interface ButtonProps {
 		children?: React.ReactNode;
+		style?: React.CSSProperties;
+		onClick?: React.MouseEventHandler<HTMLElement>;
+		icon?: React.ReactNode;
 		type?: "primary" | "ghost" | "dashed" | "link" | "text" | "default";
 		htmlType?: "button" | "submit" | "reset";
 		loading?: boolean;
 		block?: boolean;
 		size?: "large" | "middle" | "small";
+		danger?: boolean;
+		title?: string;
+	}
+
+	export interface ModalProps {
+		title?: ReactNode;
+		open?: boolean;
+		onCancel?: (e: React.MouseEvent<HTMLElement>) => void;
+		footer?: ReactNode | null;
+		destroyOnClose?: boolean;
+		children?: ReactNode;
+	}
+
+	export interface PopconfirmProps {
+		title?: ReactNode;
+		onConfirm?: (e?: React.MouseEvent<HTMLElement>) => void;
+		onCancel?: (e?: React.MouseEvent<HTMLElement>) => void;
+		okText?: ReactNode;
+		cancelText?: ReactNode;
+		children?: ReactNode;
 	}
 
 	export interface InputProps {
@@ -43,16 +67,86 @@ declare module "antd" {
 		ref?: React.Ref<HTMLInputElement>;
 	}
 
+	export interface InputNumberProps {
+		placeholder?: string;
+		style?: React.CSSProperties;
+		min?: number;
+		max?: number;
+		value?: number;
+		defaultValue?: number;
+		onChange?: (value: number | null) => void;
+		onBlur?: React.FocusEventHandler<HTMLInputElement>;
+		disabled?: boolean;
+		step?: number;
+		name?: string;
+		ref?: React.Ref<HTMLInputElement>;
+	}
+
 	export interface InputPasswordProps extends InputProps {
 		visibilityToggle?: boolean;
 	}
 
+	export interface TextAreaProps {
+		rows?: number;
+		placeholder?: string;
+		onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
+		onBlur?: React.FocusEventHandler<HTMLTextAreaElement>;
+		value?: string;
+		style?: React.CSSProperties;
+		disabled?: boolean;
+		name?: string;
+		ref?: React.Ref<HTMLTextAreaElement>;
+		allowClear?: boolean;
+		showCount?: boolean;
+		maxLength?: number;
+	}
+
 	const Input: React.FC<InputProps> & {
-		Password: React.FC<PasswordProps>;
+		Password: React.FC<InputPasswordProps>;
+		InputNumber: React.FC<InputNumberProps>;
 		Group: React.FC<any>;
 		Search: React.FC<any>;
-		TextArea: React.FC<any>;
+		TextArea: React.FC<TextAreaProps>;
 	};
 
 	export default Input;
+
+	export interface TagProps {
+		children?: ReactNode;
+		closable?: boolean;
+		onClose?: (e: React.MouseEvent<HTMLElement>) => void;
+		style?: React.CSSProperties;
+		color?: string;
+		icon?: ReactNode;
+	}
+
+	export const Tag: React.FC<TagProps> & {
+		CheckableTag: React.FC<TagProps>;
+	};
+
+	export interface SelectProps {
+		children?: ReactNode;
+		style?: React.CSSProperties;
+		placeholder?: string;
+		onChange?: (value: any, option: ReactNode | ReactNode[]) => void;
+		value?: any;
+		defaultValue?: any;
+		options?: Array<SelectOptionProps>;
+	}
+
+	export interface SelectOptionProps {
+		children?: ReactNode;
+		value?: any;
+		disabled?: boolean;
+		key?: string | number;
+		className?: string;
+		style?: React.CSSProperties;
+		title?: string;
+		label?: ReactNode;
+	}
+
+	export const Select: React.FC<SelectProps> & {
+		Option: React.FC<SelectOptionProps>;
+		OptGroup: React.FC<any>;
+	};
 }
