ca13b70c36e663668c7fbf1550b3f1f7122fe2ca
Amanda Dolci <amanda.dolci@gmail.com>
2025-04-10 16:04:58 -0300

    Cria arquivo css com variáveis globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "antd/dist/reset.css";
+import "./index.css";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { UserProvider } from "./providers/UserContext.tsx";
