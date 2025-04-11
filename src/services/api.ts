aa8bf03513bb1c3d143df5d69db463b7e492280f
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-04-11 13:50:58 -0300

    Atualiza baseURL: "http://localhost:3000/v1"

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/v1",
   timeout: 5000,
 });
